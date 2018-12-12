4215fe19b1db6cca4caf45b59b009bb42cb61707
sushantlp <sushantsingh.1081@gmail.com>
2018-12-13 00:01:51 +0530

    Dummy project

--- next.config.js
+++ next.config.js
@@ -1,10 +1,36 @@
-module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+require("dotenv").config();
 
-    return config
+const withCss = require("@zeit/next-css");
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = withCss({
+  webpack(config) {
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, ".env"),
+        systemvars: true
+      })
+    ];
+
+    config.module.rules.push({
+      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+          publicPath: "./",
+          outputPath: "static/",
+          name: "[name].[ext]"
+        }
+      }
+    });
+
+    return config;
   }
-}
+});
--- pages/index.js
+++ pages/index.js
@@ -1,91 +1,86 @@
-import React from 'react'
-import Link from 'next/link'
-import Head from '../components/head'
-import Nav from '../components/nav'
+import React from "react";
+import Link from "next/link";
+import "semantic-ui-css/semantic.min.css";
+import { Modal, Header, Button, List, Icon } from "semantic-ui-react";
 
-const Home = () => (
-  <div>
-    <Head title="Home" />
-    <Nav />
+import { connect } from "react-redux";
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+import { loadData, startClock, tickClock } from "../actions";
+import Page from "../components/page";
 
-      <div className="row">
-        <Link href="https://github.com/zeit/next.js#getting-started">
-          <a className="card">
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next on Github and in their examples</p>
-          </a>
-        </Link>
-        <Link href="https://open.segment.com/create-next-app">
-          <a className="card">
-            <h3>Examples &rarr;</h3>
-            <p>
-              Find other example boilerplates on the{' '}
-              <code>create-next-app</code> site
-            </p>
-          </a>
-        </Link>
-        <Link href="https://github.com/segmentio/create-next-app">
-          <a className="card">
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it</p>
-          </a>
-        </Link>
-      </div>
-    </div>
+class Index extends React.Component {
+  static async getInitialProps(props) {
+    const { store, isServer } = props.ctx;
+    store.dispatch(tickClock(isServer));
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+    if (!store.getState().placeholderData) {
+      store.dispatch(loadData());
+    }
 
-export default Home
+    return { isServer };
+  }
+
+  componentDidMount() {
+    this.props.dispatch(startClock());
+  }
+
+  render() {
+    return <Page title="Index Page" linkTo="/other" NavigateTo="Other Page" />;
+  }
+}
+
+export default connect()(Index);
+
+// export default () => (
+//   // <ul>
+//   //   <li>
+//   //     <Link href="/b" as="/a">
+//   //       <a>a</a>
+//   //     </Link>
+//   //   </li>
+//   //   <li>
+//   //     <Link href="/a" as="/b">
+//   //       <a>b</a>
+//   //     </Link>
+//   //   </li>
+//   //   <li>
+//   //     <Link href={{ pathname: "/posts", query: { id: "2" } }} as="/posts/2">
+//   //       <a>post #2</a>
+//   //     </Link>
+//   //   </li>
+//   // </ul>
+//   <div>
+//     <div>{process.env.TEST}</div>
+//     <Modal trigger={<Button>Show Modal</Button>}>
+//       <Modal.Header>Select a Photo</Modal.Header>
+//       <Modal.Content image>
+//         <Modal.Description>
+//           <Header>Default Profile Image</Header>
+//           <p>
+//             We have found the following gravatar image associated with your
+//             e-mail address.
+//           </p>
+//           <p>Is it okay to use this photo?</p>
+//         </Modal.Description>
+//       </Modal.Content>
+//     </Modal>
+//     <List relaxed>
+//       <List.Item>
+//         <List.Content>
+//           <List.Header as="a">Next.js</List.Header>
+//         </List.Content>
+//       </List.Item>
+//       <List.Item>
+//         <List.Content>
+//           <List.Header as="a">React</List.Header>
+//         </List.Content>
+//       </List.Item>
+//       <List.Item>
+//         <List.Content>
+//           <List.Header as="a">Vue.js</List.Header>
+//         </List.Content>
+//       </List.Item>
+//     </List>
+//     Hello <Icon name="world" />
+//   </div>
+// );
