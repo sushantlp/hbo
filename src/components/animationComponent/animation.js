c7005e4dd93de260c72f6c6e8bc9680c29f1403c
sushantlp <sushantsingh.1081@gmail.com>
2018-12-21 19:56:40 +0530

    Animation done

--- src/components/animationComponent/animation.js
+++ src/components/animationComponent/animation.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Container } from "semantic-ui-react/dist/commonjs";
+import { Container, Grid } from "semantic-ui-react/dist/commonjs";
 import { Animated } from "react-animated-css";
 
 import "./static/css/animation.css";
@@ -8,67 +8,104 @@ import "./static/css/animation.css";
 export default class Animation extends React.Component {
   render() {
     return (
-      <Container style={{ overflow: "hidden" }}>
-        <Animated
-          animationIn="slideInLeft"
-          animationOut="pulse"
-          isVisible={true}
-        >
-          <div className="square1">
-            <span className="Span">Hover</span>
-          </div>
-        </Animated>
+      <Container fluid>
+        <Grid columns="equal">
+          <Grid.Row>
+            <Grid.Column>
+              <Animated
+                animationIn="bounceInLeft"
+                animationOut="pulse"
+                isVisible={true}
+              >
+                <div
+                  className="box"
+                  style={{ backgroundColor: "rgba(253, 187, 127, 1)" }}
+                >
+                  {/* <span className="Span">Hover</span> */}
+                </div>
+              </Animated>
 
-        <Animated animationIn="zoomIn" animationOut="pulse" isVisible={true}>
+              {/* <Animated animationIn="zoomIn" animationOut="pulse" isVisible={true}>
           <div className="circle" />
-        </Animated>
+        </Animated> */}
 
-        <Animated
-          animationIn="slideInLeft"
-          animationOut="pulse"
-          isVisible={true}
-        >
-          <div className="square2">
-            <span className="Span">Hover</span>
-          </div>
-        </Animated>
+              <Animated
+                animationIn="bounceInLeft"
+                animationOut="pulse"
+                isVisible={true}
+              >
+                <div className="box" style={{ backgroundColor: "#ff1c68" }}>
+                  {/* <span className="Span">Hover</span> */}
+                </div>
+              </Animated>
 
-        <Animated
-          animationIn="slideInLeft"
-          animationOut="pulse"
-          isVisible={true}
-        >
-          <div className="square3">
-            <span className="Span">Hover</span>
-          </div>
-        </Animated>
-        <Animated
-          animationIn="slideInRight"
-          animationOut="pulse"
-          isVisible={true}
-        >
-          <div className="square4">
-            <span className="Span">Right-Hover</span>
-          </div>
-        </Animated>
-        <Animated
-          animationIn="slideInRight"
-          animationOut="pulse"
-          isVisible={true}
-        >
-          <div className="square5">
-            <span className="Span">Right-Hover</span>
-          </div>
-        </Animated>
-        <Animated
-          animationIn="slideInRight"
-          animationOut="pulse"
-          isVisible={true}
-        >
-          <div className="square6">
-            <span className="Span">Right-Hover</span>
-          </div>
-        </Animated>
+              <Animated
+                animationIn="bounceInLeft"
+                animationOut="pulse"
+                isVisible={true}
+              >
+                <div className="box" style={{ backgroundColor: "#87cdab" }}>
+                  {/* <span className="Span">Hover</span> */}
+                </div>
+              </Animated>
+            </Grid.Column>
+            <Grid.Column>
+              <Animated
+                animationIn="zoomIn"
+                animationOut="pulse"
+                isVisible={true}
+              >
+                <div className="circle" style={{ backgroundColor: "white" }} />
+              </Animated>
+              <Animated
+                animationIn="zoomIn"
+                animationOut="pulse"
+                isVisible={true}
+              >
+                <div
+                  className="circle"
+                  style={{ backgroundColor: "#87cdab" }}
+                />
+              </Animated>
+              <Animated
+                animationIn="zoomIn"
+                animationOut="pulse"
+                isVisible={true}
+              >
+                <div className="circle" style={{ backgroundColor: "white" }} />
+              </Animated>
+            </Grid.Column>
+            <Grid.Column>
+              <Animated
+                animationIn="bounceInRight"
+                animationOut="pulse"
+                isVisible={true}
+              >
+                <div className="box" style={{ backgroundColor: "#9db4d6" }}>
+                  {/* <span className="Span">Right-Hover</span> */}
+                </div>
+              </Animated>
+              <Animated
+                animationIn="bounceInRight"
+                animationOut="pulse"
+                isVisible={true}
+              >
+                <div className="box" style={{ backgroundColor: "#a585bd" }}>
+                  {/* <span className="Span">Right-Hover</span> */}
+                </div>
+              </Animated>
+              <Animated
+                animationIn="bounceInRight"
+                animationOut="pulse"
+                isVisible={true}
+              >
+                <div className="box" style={{ backgroundColor: "#e48a84" }}>
+                  {/* <span className="Span">Right-Hover</span> */}
+                </div>
+              </Animated>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
     );
   }
